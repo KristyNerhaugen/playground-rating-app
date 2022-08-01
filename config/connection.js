9a016236ed1de25f0796d50f5f49380725e52f09
kristynerhaugen <kristynerhaugen@gmail.com>
2022-08-01 11:59:33 -0500

    server set up

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// importing sequelize
+const Sequelize = require("sequelize");
+
+// require dotenv dependency
+require("dotenv").config();
+
+// create connection to database
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+
+module.exports = sequelize;
