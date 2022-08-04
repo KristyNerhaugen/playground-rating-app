6243aba26a379745823f7e9b00844dbb2fe61dfc
kristynerhaugen <kristynerhaugen@gmail.com>
2022-08-04 09:03:20 -0500

    user routes started

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+// route to get all users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// route to get user by id
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "park_name", "location", "review"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user is found with this id." });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// post user route
+
+// post user login route
+
+// post user logout route
+
+// put user route to update by id
+
+// delete user by id route
