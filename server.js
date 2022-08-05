ef6aa002a276e8a5313ea184c6e8479e3926c66f
kaylab78 <kaylabackus78@gmail.com>
2022-08-05 14:43:21 -0500

    Added login/sign up template.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 // const helpers = require("./utils/helpers");
-const hbs = exphbs.create({  });
+const hbs = exphbs.create({});
 const routes = require("./controllers");
 
 const app = express();
