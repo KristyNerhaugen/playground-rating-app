f050b9bbf8da7beb702ce2ca2dd55b0cf5e1f27a
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-08-06 11:44:07 -0500

    fixed naming relation to input fields

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,9 +1,9 @@
 async function newFormHandler(event) {
   event.preventDefault();
 
-  const park_name = document.querySelector('input[name="park-name"]').value;
-  const location = document.querySelector('input[name="location"]').value;
-  const review = document.querySelector('textarea[name="review"]').value;
+  const park_name = document.querySelector('input[id="park-name"]').value;
+  const location = document.querySelector('input[id="location"]').value;
+  const review = document.querySelector('textarea[id="review"]').value;
 
   const response = await fetch(`/api/posts`, {
     method: 'POST',
